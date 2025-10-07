8533a57eb937004548b632e9347c8539b876a28b
Mohammad Jashem <mjashem@hotmail.com>
2025-10-07 14:37:43 +0600

    fix mobile layout

--- src/components/HeroAnimated.tsx
+++ src/components/HeroAnimated.tsx
@@ -16,7 +16,7 @@ const HeroAnimated: React.FC<HeroAnimatedProps> = ({ className = '' }) => {
       {/* Background with decorative elements */}
       <div className="absolute inset-0">
         {/* Wavy divider at bottom */}
-        <div className="absolute bottom-0 left-0 right-0 z-10" style={{ height: '23px' }}>
+        <div className="absolute -bottom-2 lg:-bottom-0 left-0 right-0 z-10" style={{ height: '23px' }}>
           <svg viewBox="0 0 1440 23" className="w-full h-full" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path
               d="M1440 0.5L0 22.5H1440V0.5Z"
@@ -26,15 +26,15 @@ const HeroAnimated: React.FC<HeroAnimatedProps> = ({ className = '' }) => {
         </div>
       </div>
 
-      <div className="relative h-full pt-10 lg:pt-16">
+      <div className="relative h-full pt-2 lg:pt-16">
         <div className="mx-auto px-4 sm:px-6 lg:px-8 max-w-6xl h-full">
-          <div className="flex flex-col lg:flex-row items-start lg:items-start justify-between h-full w-full gap-8 lg:gap-16">
+          <div className="flex flex-col lg:flex-row items-start lg:items-start justify-between h-full w-full gap-0 lg:gap-16">
 
             {/* Hero Content */}
-            <AnimatedSection animation="slideRight" delay={0.2} className="flex-1 lg:flex-[2] text-center lg:text-left order-2 lg:order-1">
+            <AnimatedSection animation="slideRight" delay={0.2} className="flex-1 lg:flex-[2] text-center lg:text-left order-1 lg:order-1">
               <div className="space-y-6">
                 {/* Decorative elements row */}
-                <div className="flex flex-row items-center lg:items-start gap-4 justify-end lg:justify-end -mb-8 mr-44">
+                <div className="flex flex-row items-center lg:items-start gap-4 justify-end lg:justify-end -lg:mb-8 -mb-2 mr-3 lg:mr-44">
                   <div className="flex h-[60px] sm:h-[80px] lg:h-[100px] items-center justify-center w-[40px] sm:w-[45px] lg:w-[50px]" style={{ transform: 'rotate(13.97deg)' }}>
                     <img
                       src="/b2de00c26e68f0cd06db557a2c071e420ab33f7c.svg"
@@ -100,15 +100,15 @@ const HeroAnimated: React.FC<HeroAnimatedProps> = ({ className = '' }) => {
                 </motion.button>
 
                 {/* App Store Badges */}
-                <div className="flex gap-3">
-                  <div className="w-[162px] h-12">
+                <div className="flex gap-3 w-full sm:w-auto">
+                  <div className="flex-1 sm:flex-initial sm:w-[162px] h-12">
                     <img
                       src="/50ef2ac624889246853661a08035409d6146ae4d.svg"
                       alt="App Store"
                       className="w-full h-full object-contain"
                     />
                   </div>
-                  <div className="w-[162px] h-12">
+                  <div className="flex-1 sm:flex-initial sm:w-[162px] h-12">
                     <img
                       src="/91d03ea036776e5d506d4a563368f57e1012ce70.svg"
                       alt="Google Play"
@@ -116,7 +116,7 @@ const HeroAnimated: React.FC<HeroAnimatedProps> = ({ className = '' }) => {
                     />
                   </div>
                 </div>
-                <div className="flex items-center justify-center w-[45px] sm:w-[50px] lg:w-[55px] h-[45px] sm:h-[50px] lg:h-[55px] ml-18 mt-12">
+                <div className="hidden lg:flex items-center justify-center w-[45px] sm:w-[50px] lg:w-[55px] h-[45px] sm:h-[50px] lg:h-[55px] ml-18 mt-12">
                     <img
                       src="/c068262c56f0183e00a6b2dfa618608bda1d6964.svg"
                       alt="Decorative element 3"
@@ -128,17 +128,19 @@ const HeroAnimated: React.FC<HeroAnimatedProps> = ({ className = '' }) => {
 
             {/* iPhone Mockup */}
             <motion.div
-              className="flex-1 lg:flex-[1] flex justify-center lg:justify-end order-1 lg:order-2 mt-6 relative z-20"
+              className="flex-1 lg:flex-[1] flex justify-center lg:justify-end order-1 lg:order-2 lg:mt-6 relative z-20"
               initial={{ opacity: 0, x: 100 }}
               animate={{ opacity: 1, x: 0 }}
               transition={{ duration: 1, delay: 0.6, ease: "easeOut" }}
             >
-              <div className="relative scale-75 sm:scale-90 lg:scale-100 origin-top" style={{ marginBottom: '-80px' }}>
+              <div className="relative w-full lg:w-auto origin-top" style={{ marginBottom: '-80px' }}>
                 <img
                   src="/iPhone-11.png"
                   alt="iPhone 11 with Kahf Kids App"
-                  className="w-auto h-auto max-w-full"
-                  style={{ maxHeight: '600px' }}
+                  className="w-full h-auto object-contain lg:w-auto lg:h-auto"
+                  style={{
+                    maxHeight: '600px'
+                  }}
                 />
               </div>
             </motion.div>
